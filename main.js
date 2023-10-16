82366e6c89d033c8a161722b609eefe293dcf0c7
Isabelle <isabelle.sj@live.se>
2023-10-16 15:52:13 +0200

    add removing p tags and filling titles and images

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ fetch("http://omdbapi.com/?apikey=9a863550&s=harry")
 
 function createHTML(movies) {
   const moviesContainer = document.getElementById("movies");
-  // moviesContainer.innerHTML = "";
 
   for (let i = 0; i < movies.length; i++) {
     const movieContainer = document.createElement("div");
@@ -15,13 +14,12 @@ function createHTML(movies) {
     const imgContainer = document.createElement("div");
     imgContainer.className = "imgContainer";
     const movieImg = document.createElement("img");
+    movieImg.setAttribute("id", "movieImg-" + i.toString());
     const title = document.createElement("h3");
+    title.setAttribute("id", "title-" + i.toString());
 
-    const movieTitle = movies[i].Title;
-    const moviePoster = movies[i].Poster;
-
-    title.innerHTML = movieTitle;
-    movieImg.src = moviePoster;
+    title.innerHTML = movies[i].Title;
+    movieImg.src = movies[i].Poster;
 
     moviesContainer.appendChild(movieContainer);
     imgContainer.appendChild(movieImg);
@@ -29,24 +27,46 @@ function createHTML(movies) {
     movieContainer.appendChild(imgContainer);
 
     movieContainer.addEventListener("click", () => {
-      movieInfo(movies[i].imdbID, movieContainer);
+      fillTitlesAndImages(movies);
+      movieInfo(movies[i].imdbID, title, movieImg, movieContainer);
     });
   }
 }
 
-function movieInfo(imdbID, movieContainer, movies) {
+function movieInfo(imdbID, title, movieImg, movieContainer) {
   fetch(`http://omdbapi.com/?apikey=9a863550&i=${imdbID}`)
     .then((response) => response.json())
     .then((movieInfo) => {
       console.log(movieInfo);
-      createHtmlForMovie(movieInfo, movieContainer, movies);
+      createHtmlForMovie(movieInfo, title, movieImg, movieContainer);
     });
 }
 
-function createHtmlForMovie(movieInfo, movieContainer, movies) {
-  movieContainer.innerHTML = "";
+function createHtmlForMovie(movieInfo, title, movieImg, movieContainer) {
+  title.innerHTML = "";
+  movieImg.src = "";
+
   const textInfo = document.createElement("p");
   textInfo.innerHTML = movieInfo.Plot;
 
   movieContainer.appendChild(textInfo);
+}
+
+function fillTitlesAndImages(movies) {
+  removePtags();
+
+  for (let i = 0; i < movies.length; i++) {
+    const title = document.getElementById("title-" + i.toString());
+    const movieImg = document.getElementById("movieImg-" + i.toString());
+
+    title.innerHTML = movies[i].Title;
+    movieImg.src = movies[i].Poster;
+  }
+}
+
+function removePtags() {
+  const pTags = document.getElementsByTagName("p");
+  for (let i = 0; i < pTags.length; i++) {
+    pTags[i].remove();
+  }
 }
